8d1d4e57da321e45d8f8dd19c8e968cc1a24586a
zhuyc <zyc199777@outlook.com>
2020-04-15 18:08:25 +0800

    整合 mybatis

---
+++ atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/main/test/java/CrowdSpringTest.java
@@ -0,0 +1,61 @@
+
+import com.atguigu.crowd.entity.AdminEntity;
+import com.atguigu.crowd.mapper.AdminMapper;
+import com.atguigu.crowd.service.impl.AdminServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @author <a href="zyc199777@gmail.com">Zhu yc</a>
+ * @version 1.0
+ * @date 2020年04月15日
+ * @desc CrowdSpringTest
+ */
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring.xml"})
+public class CrowdSpringTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private AdminMapper mapper;
+
+    /*@Autowired
+    private AdminServiceImpl service;*/
+
+    @Test
+    public void testDataSource() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+    }
+
+    @Test
+    public void testMapper(){
+//        AdminEntity entity = new AdminEntity();
+        /*entity.setId(1)
+                .setLoginAcct("test_mapper")
+                .setUserPswd("test_mapper")
+                .setUserName("test_mapper")
+                .setEmail("test_mapper");*/
+//        mapper.insert(entity);
+//         mapper.updateById(entity);
+        System.out.println(mapper.selectById(1));
+    }
+
+    @Test
+    public void testLog(){
+        log.debug("debug message");
+        log.info("info message");
+        log.error("error message");
+    }
+}
--- atcrowdfunding01-admin-parent/atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/controller/AdminController.java
+++ atcrowdfunding01-admin-parent/atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/controller/AdminController.java
@@ -2,7 +2,6 @@ package com.atguigu.crowd.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 /**
